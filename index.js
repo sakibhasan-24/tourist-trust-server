f1c835a1bf73353583e630164d846ed6ea95c822
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-01-18 02:25:08 +0600

    create a tourist

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.port || 5000;
 
+app.use(cors());
+app.use(express.json());
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 console.log(process.env.MONGO_URL);
 const client = new MongoClient(process.env.MONGO_URL, {
@@ -16,7 +19,14 @@ const client = new MongoClient(process.env.MONGO_URL, {
 });
 
 async function run() {
+  const touristCollection = client.db("tourist-trust").collection("tourist");
+
   try {
+    app.post("/tourist-list", async (req, res) => {
+      const data = req.body;
+      const result = await touristCollection.insertOne(data);
+      res.send(result);
+    });
   } finally {
   }
 }
